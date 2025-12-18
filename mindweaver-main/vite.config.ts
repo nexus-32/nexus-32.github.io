67fa72ecf5867dc71b43da3a678d9f4853dd544d
unknown <gromov.gamedeveloper@gmail.com>
2025-12-18 13:03:09 +1000

    Configure project for GitHub Pages deployment

--- mindweaver-main/vite.config.ts
+++ mindweaver-main/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/mindweaver-ai/" : "/",
   server: {
     host: "::",
     port: 8080,
